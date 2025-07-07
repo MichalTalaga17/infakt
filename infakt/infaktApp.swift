321ed4902dd2369b7ffa73fea5b9472d6ca911ed
Michał Talaga <michal.talaga.programming@gmail.com>
2025-07-07 16:31:39 +0200

    Initial Commit

---
+++ infakt/infaktApp.swift
@@ -0,0 +1,17 @@
+//
+//  infaktApp.swift
+//  infakt
+//
+//  Created by Michał Talaga on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct infaktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
