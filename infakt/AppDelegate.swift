f654fb03783d8a922c162b727018d9a499cab763
Michał Talaga <michal.talaga.programming@gmail.com>
2025-07-08 23:42:20 +0200

    push

---
+++ infakt/AppDelegate.swift
@@ -0,0 +1,17 @@
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            } else {
+                print("Permission for push notifications denied.")
+            }
+        }
+        return true
+    }
+}
